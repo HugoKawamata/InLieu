140f586be12515f6f7c42a1ac32f4556a5ff1d31
Hugo Kawamata <hugokawamata@gmail.com>
2017-11-25 22:00:00 +1000

    Get google maps api rendering a map

--- src/Components/Find.tsx
+++ src/Components/Find.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import * as firebase from "firebase";
+import { withProps, compose } from "recompose";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import { Segment, Input, Button } from "semantic-ui-react";
 
 interface Props {
@@ -9,11 +11,35 @@ interface State {
 
 }
 
+const MapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry," +
+    "drawing,places&key=AIzaSyDaSKRHqKVoX30QjzSHFRYupe92K_NpJpk",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) => (
+  <GoogleMap
+  defaultZoom={8}
+  defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+
+  {true && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+));
+
+const SMapComponent = "Hello";
+
 export default class Find extends React.Component<Props, State> {
   render() {
     return (
       <Segment attached="bottom">
         <div className="map">
+          yeet
+          {<MapComponent/>}
         </div>
       </Segment>
     );
